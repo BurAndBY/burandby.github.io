d91d14ed860a50c8fbdd9fe8119cde3a46cd8366
BurAndBY <48630651+BurAndBY@users.noreply.github.com>
2023-06-07 21:15:10 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 var app = document.getElementById('app');
 
 var typewriter = new Typewriter(app, {
-  loop: true,
-  delay: 75,
+  loop: false,
+  delay: 25,
   autoStart: true,
   cursor: '█',
   strings: ["Hi! I'm Bur, but you probably know me under the username BurAndBY", "I've worked on different sites. You can find them in the /mysites", "I'm also a big retro geek, that's why this site ended up looking like this"]
